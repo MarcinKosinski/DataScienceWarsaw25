ac42502db6f984378161a4f9bfb4bbffc8c222b6
krzyslom <krzysztofslomczynski@gmail.com>
2017-04-07 19:19:04 +0200

    #1 Opinie

--- run_cron.sh
+++ run_cron.sh
@@ -4,4 +4,5 @@ bash -c "cd /home/krzysztof/GitHub/DataScienceWarsaw25 && \
 /usr/bin/Rscript /home/krzysztof/GitHub/DataScienceWarsaw25/wp_facet.R && \
 /usr/bin/Rscript /home/krzysztof/GitHub/DataScienceWarsaw25/wp_gwiazdy.R && \
 /usr/bin/Rscript /home/krzysztof/GitHub/DataScienceWarsaw25/wp_moto.R && \
-/usr/bin/Rscript /home/krzysztof/GitHub/DataScienceWarsaw25/wp_tech.R"
+/usr/bin/Rscript /home/krzysztof/GitHub/DataScienceWarsaw25/wp_tech.R && \
+/usr/bin/Rscript /home/krzysztof/GitHub/DataScienceWarsaw25/wp_opinie.R"
