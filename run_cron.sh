092f0014976fd46707f6cc5345b05ff1bab5ea8e
krzyslom <krzysztofslomczynski@gmail.com>
2017-04-07 16:35:05 +0200

    #1 Kobieta

--- run_cron.sh
+++ run_cron.sh
@@ -1,2 +1,3 @@
 bash -c "cd /home/krzysztof/GitHub/DataScienceWarsaw25 && \
-/usr/bin/Rscript /home/krzysztof/GitHub/DataScienceWarsaw25/wp_wiadomosci.R"
+/usr/bin/Rscript /home/krzysztof/GitHub/DataScienceWarsaw25/wp_wiadomosci.R && \
+/usr/bin/Rscript /home/krzysztof/GitHub/DataScienceWarsaw25/wp_kobieta.R"
--- wp_db.R
+++ wp_db.R
@@ -8,11 +8,13 @@ library(stringi)
 
 db <- dbConnect(drv = SQLite(), dbname = "data/wp.db")
 
-tab_name <- "wp_wiadomosci"
+tab_name <- "wp_kobieta"
 template <- paste0("data/", tab_name, ".csv") %>%
   read_csv()
 
-dbSendQuery(conn = db,"CREATE TABLE wp_wiadomosci (links TEXT, bodies TEXT)")
+dbGetQuery(db, "DROP TABLE wp_kobieta")
+# dbSendQuery(conn = db, paste0("CREATE TABLE ", tab_name,
+#                               " (links TEXT, bodies TEXT)"))
 dbWriteTable(db, name = tab_name, value = template)
 
 dbDisconnect(db)
